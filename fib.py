fa23262ac425191602b5a0fb64ecd7753c6ff7e7
Michael Fang <1michaelfang@gmail.com>
2017-05-28 16:34:41 -0700

    Added param for init values

--- fib.py
+++ fib.py
@@ -1,10 +1,11 @@
 import numpy as np
 
-def fib(n):
+def fib(n, x0 = 0, x1 = 1):
     if n <= 0:
-        return 0
+        return x0
     if n == 1:
-        return 1
+        return x1
     return fib(n-1) + fib(n-2)
 
 print [ fib(n) for n in range(10) ]
+print [ fib(n, 1, 1) for n in range(10) ]
