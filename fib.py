8a2c27ae5c2492eb6f90f02fb83bdc5c90a19668
Michael Fang <1michaelfang@gmail.com>
2017-05-28 16:41:16 -0700

    Revert "Added param for init values"
    This reverts commit fa23262ac425191602b5a0fb64ecd7753c6ff7e7.

--- fib.py
+++ fib.py
@@ -1,11 +1,10 @@
 import numpy as np
 
-def fib(n, x0 = 0, x1 = 1):
+def fib(n):
     if n <= 0:
-        return x0
+        return 0
     if n == 1:
-        return x1
+        return 1
     return fib(n-1) + fib(n-2)
 
 print [ fib(n) for n in range(10) ]
-print [ fib(n, 1, 1) for n in range(10) ]
